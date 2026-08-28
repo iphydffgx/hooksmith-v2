f89d1595a673507f2a3a57466bea451a90d602b5
iphydffgx <KanyeMaday432167@hotmail.com>
2026-08-29 00:03:35 +0800

    initial commit

---
+++ src/useDebounce.js
@@ -0,0 +1,22 @@
+// TODO: edge cases later
+import { useEffect, useRef, useState } from 'react';
+
+// debounce a changing value; trailing edge by default
+export function useDebounce(value, delay = 300, { leading = false } = {}) {
+  const [debounced, setDebounced] = useState(value);
+  const leadingFired = useRef(false);
+
+  useEffect(() => {
+    if (leading && !leadingFired.current) {
+      leadingFired.current = true;
+      setDebounced(value);
+    }
+    const t = setTimeout(() => {
+      setDebounced(value);
+      leadingFired.current = false;
+    }, delay);
+    return () => clearTimeout(t);
+  }, [value, delay, leading]);
+
+  return debounced;
+}
